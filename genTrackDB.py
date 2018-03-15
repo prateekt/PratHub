7b29d4b71c5aeb0b4b7e3196cd9dc94e027e1580
prtandon <prtandon@ucsd.edu>
2018-03-14 18:57:35 -0700

    test run

---
+++ genTrackDB.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+def rootname(f):
+	a = os.path.basename(f)
+	aParts = a.split('.')
+	return aParts[0]
+
+def makeTrackStr(f,tag):
+	str = (	
+		'track ' + tag + '\n'
+		'bigDataUrl ' + f + '\n'
+		'shortLabel ' + tag + '\n'
+		'longLabel '  +tag + '\n'
+		'type bigBed 5\n'
+		'spectrum on\n\n\n'
+	       )
+	return str
+
+#get top folders
+top_folders = [a for a in os.listdir('hg19') if os.path.isdir('hg19/' + a)]
+
+#folder iter
+fout = open('hg19/trackDb.txt','w')
+for f in top_folders:
+	files = ['hg19/' + f + '/' + a for a in os.listdir('hg19/' + f) if ".bb" in a]
+	for urlIndex in xrange(0,len(files)):
+		url = files[urlIndex]
+		command = makeTrackStr(url,f+'-'+rootname(url))
+		if(urlIndex==len(files)-1):
+			command = command.rstrip()
+		fout.write(command)
+fout.close()
